9769daede505bfc0c9a6c49876c8015e82b4bf87
rhanziy <thdfksdl0029@gmail.com>
2024-05-31 19:20:41 +0900

    fix: gradle setting

--- android/app/src/main/java/com/reretsproject/MainApplication.kt
+++ android/app/src/main/java/com/reretsproject/MainApplication.kt
@@ -20,7 +20,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
-              new RNCConfigPackage()
+//              new RNCConfigPackage()  
             }
 
         override fun getJSMainModuleName(): String = "index"
